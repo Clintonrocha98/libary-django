3b950e2cb40b509e8c2ea8a06f4266dad40db8fb
Clintonrocha98 <clintongoncalvesrocha@gmail.com>
2024-08-17 21:06:25 -0300

    feat: create urls for the biblioteca project and add in the core

--- biblioteca/biblioteca/urls.py
+++ biblioteca/biblioteca/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import include, path
+from core.urls import urlpatterns as core_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(core_urls))
 ]
